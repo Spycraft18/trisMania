1289e5c4c4961e01d94e168f3ecf32d505655f83
Kenerth <kenerthcarrion1900@gmail.com>
2024-06-20 14:43:59 -0600

    minuto 28

--- Assets/scripts/Tetromino.cs
+++ Assets/scripts/Tetromino.cs
@@ -1,5 +1,5 @@
+using UnityEngine;
 using UnityEngine.Tilemaps;
-
 public enum Tetromino
 {
     I,
@@ -16,4 +16,9 @@ public struct TetrominoData
 {
     public Tetromino tetromino;
     public Tile tile;
+    public Vector2Int[] cells { get;  private set; }
+    public void Initialize()
+    {
+        this.cells = Data.Cells[this.tetromino];
+    }
 }
--- Assets/scripts/board.cs
+++ Assets/scripts/board.cs
@@ -1,6 +1,13 @@
 using UnityEngine ;
-
+using UnityEngine.Tilemaps;
 public class board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
     public TetrominoData[] tetrominoes;
+    private void awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        for (int i = 0; i < this.tetrominoes.Length; i++) ;
+        this.tetrominoes[i].Initialize();
+    }
 }

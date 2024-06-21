e08d4a83ade4a56a946693d98a7de1a41f0e6a3a
Jahir <spycraft18@gmail.com>
2024-06-20 23:38:29 -0600

    Hecho Seccion de Spawn de bloques Minuto 43

--- Assets/scripts/board.cs
+++ Assets/scripts/board.cs
@@ -1,12 +1,15 @@
 using UnityEngine ;
 using UnityEngine.Tilemaps;
-public class board : MonoBehaviour
+public class Board : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece {  get; private set; } 
     public TetrominoData[] tetrominoes;
-    private void awake()
+    public Vector3Int spawnPosition;
+    private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>(); 
         for (int i = 0; i < this.tetrominoes.Length; i++)
         {
         this.tetrominoes[i].Initialize();
@@ -22,10 +25,16 @@ public class board : MonoBehaviour
     {
         int random = Random.Range(0, this.tetrominoes.Length);
         TetrominoData data = this.tetrominoes[random];
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-
+        for (int i = 0; i < piece.cells.Length; i++) 
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 }

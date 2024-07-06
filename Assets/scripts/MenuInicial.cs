756b18dd628ae0f3b7e60cbe4b90d91dbb337f71
Jahir <spycraft18@gmail.com>
2024-07-05 22:11:13 -0600

    A POR GAME OVER

--- Assets/scripts/MenuInicial.cs
+++ Assets/scripts/MenuInicial.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuInicial : MonoBehaviour
 {
+    public Pause pause {  get; private set; }
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Tetris");
     }
     
     public void Guia()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); 
+        SceneManager.LoadScene("Guia"); 
     }
 
     public void ReturnMenu()
     {
         SceneManager.LoadScene("Menu Inicial");
+        Time.timeScale = 1;
     }
     public void Salir()
     {
